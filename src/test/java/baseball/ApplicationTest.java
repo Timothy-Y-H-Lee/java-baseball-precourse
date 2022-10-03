54d78ed686421d1418f0b640b7f63e51949a855d
Timothy <timothy@jungnanum.com>
2022-10-03 19:52:39 +0900

    test: 볼, 스트라이크 연산 결과를 담은 배열 생성 test case
    상대방(컴퓨터)가 가진 야구게임 값 List와
    Player가 가진 야구게임 값 List를 서로 비교해서
    야구게임의 볼, 스트라이크 연산 결과를 담은 배열을 생성하는 test case
    
    BREAKING CHANGE: List를_서로_비교해서_야구게임의_결괏값_배열_생성() test case 생성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -84,6 +85,21 @@ class ApplicationTest extends NsTest {
         } while (gameNumberSet.size() < RuleVo.getGameNumbersLen());
 
         return new ArrayList<>(gameNumberSet);
+    }
+
+    @Test
+    void List를_서로_비교해서_야구게임의_결괏값_배열_생성() {
+        List<Integer> gameNumberList = new ArrayList<>(Arrays.asList(3, 6, 5)); // 상대방(컴퓨터)가 가진 야구게임 값
+        List<Integer> playerGameNumberList = new ArrayList<>(Arrays.asList(3, 5, 6)); // Player가 가진 야구게임 값
+        Integer[] gameResult = {0, 0}; // 결괏값
+        for (int i = 0; i < playerGameNumberList.size(); i++) {
+            Integer findIdx = gameNumberList.indexOf(playerGameNumberList.get(i));
+            if (findIdx == -1) continue;
+            if (findIdx != i) ++gameResult[0]; // 볼
+            if (findIdx == i) ++gameResult[1]; // 스트라이크
+        }
+        assertEquals(2, gameResult[0]); // 볼
+        assertEquals(1, gameResult[1]); // 스트라이크
     }
 
     @Override
